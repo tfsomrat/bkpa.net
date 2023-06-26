229e661154ae7201d0d075fd16d31d28d2cdfda7
somrat sorkar <themefisher.dev@gmail.com>
2023-06-26 10:54:23 +0600

    update code

--- tina/__generated__/client.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'd2dc861fc4132883be1888059046d6c281dd1284', queries });
-export default client;
-  
